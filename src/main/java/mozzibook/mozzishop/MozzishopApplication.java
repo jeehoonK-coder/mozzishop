64325220a2e4cbf97f548c03566e9c4360844444
Kang Jee Hoon <shb04016@gmail.com>
2023-03-28 20:51:57 +0900

    Initial commit

---
+++ src/main/java/mozzibook/mozzishop/MozzishopApplication.java
@@ -0,0 +1,13 @@
+package mozzibook.mozzishop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MozzishopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MozzishopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mozzibook/mozzishop/MozzishopApplicationTests.java
@@ -0,0 +1,13 @@
+package mozzibook.mozzishop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MozzishopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
